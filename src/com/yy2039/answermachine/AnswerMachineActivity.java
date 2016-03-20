e9346193e49173409fee26db777839d3f97116f1
xiaou <yyolq0410@163.com>
2016-03-20 20:35:42 +0800

    fix

--- src/com/yy2039/answermachine/AnswerMachineActivity.java
+++ src/com/yy2039/answermachine/AnswerMachineActivity.java
@@ -170,6 +170,7 @@ public class AnswerMachineActivity extends FragmentActivity
 
         IntentFilter filter3 = new IntentFilter();
         filter3.addAction( "com.action.dect.page.incoming.call" );
+        filter3.addAction( "com.action.dect.call.guardian.handing.result" );
         registerReceiver( incomingCallReceiver, filter3 );  
 
         //IntentFilter filter5 = new IntentFilter();
